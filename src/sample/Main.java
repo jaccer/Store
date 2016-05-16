8d3687a905e666e6d8920c614c17ece6262ee5ce
jaccer <jaccer@gmail.com>
2016-05-17 01:52:11 +0200

    Beginning

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,24 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Stage window;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        window = primaryStage;
+
+        BorderPane layout = new BorderPane();
+        window.setTitle("Hello World");
+        window.setScene(new Scene(layout, 300, 275));
+        window.show();
+
+        System.out.println("hi");
     }
 
 
